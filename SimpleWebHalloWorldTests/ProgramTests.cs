9286399f05ef2fe2b1e52a67445197d9ac13422d
fjalling <jakob.fjalling@gmail.com>
2020-09-09 11:57:26 +0200

    Added test proj

---
+++ SimpleWebHalloWorldTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleWebHalloWorld;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebHalloWorld.Tests
+{
+    [TestClass()]
+    public class ProgramTests : Program
+    {
+        [TestMethod()]
+        public void FakeMethodTest()
+        {
+            int i = 1;
+       
+            int a = FakeMethod(i);
+
+            Assert.AreEqual(i++, a);
+        }
+    }
+}
